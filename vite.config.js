c0b8a919c51c85daad1f536da00cf6107b2e23d6
josuanhub <urusgovx@gmail.com>
2026-06-29 22:59:29 -0400

    feat: add vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   server: {
     port: 5173,
-    host: true,
     open: true
   },
   resolve: {
     alias: {
       '@': '/src'
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          icons: ['lucide-react']
-        }
-      }
     }
   }
 })
